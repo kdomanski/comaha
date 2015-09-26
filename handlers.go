ad85af9e3b9f91a01883a44d694eadb68f2337f9
Kamil Domański <kamil@domanski.co>
2015-09-26 11:43:16 +0200

    moved stuff around

--- handlers.go
+++ handlers.go
@@ -4,13 +4,25 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/base64"
-	//"encoding/hex"
+	"encoding/xml"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/coreos/go-omaha/omaha"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+const noupdateResponse = `
+<?xml version="1.0" encoding="UTF-8"?>
+<response protocol="3.0" server="update.core-os.net">
+<daystart elapsed_seconds="0"></daystart>
+<app appid="e96281a6-d1af-4bde-9a0a-97b76e56dc57" status="ok">
+<updatecheck status="noupdate"></updatecheck>
+</app>
+</response>`
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -19,7 +31,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Infof("Someone tried to access '%s'", r.URL.String())
 }
 
-func coreosHandler(w http.ResponseWriter, r *http.Request) {
+func fileHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	fileid := r.URL.Query().Get("id")
@@ -83,4 +95,71 @@ func addPayloadHandler(w http.ResponseWriter, r *http.Request) {
 
 func shutdownHandler(w http.ResponseWriter, r *http.Request) {
 	os.Exit(0)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	log.Infof("Handling an update request from %v", r.RemoteAddr)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Error(err.Error())
+	}
+
+	var reqStructure omaha.Request
+	log.Debugf("%v", string(body[:len(body)]))
+	err = xml.Unmarshal(body, &reqStructure)
+
+	if n := len(reqStructure.Apps); n != 1 {
+		log.Errorf("Client '%v' tried to update %v services", r.RemoteAddr, n)
+		http.Error(w, "I can handle only 1 app update.", 400)
+		return
+	}
+
+	log.Debugf("%#v", reqStructure)
+	applicationUpdate(w, r, reqStructure.Apps[0])
+
+}
+
+func applicationUpdate(w http.ResponseWriter, r *http.Request, app *omaha.App) {
+	if app.Id != coreOSAppID {
+		s := fmt.Sprintf("Unknown app ID '%v'.", app.Id)
+		log.Errorf("Client '%v' tried to update service '%v'.", r.RemoteAddr, app.Id)
+		http.Error(w, s, 400)
+		return
+	}
+
+	payload := backend.GetPayload("user", "channel", "version")
+
+	size := payload.Size
+	sha1 := payload.SHA1
+	sha256 := payload.SHA256
+	id := payload.Url
+
+	resp := omaha.NewResponse("coreos-update.protonet.info")
+	newApp := resp.AddApp(coreOSAppID)
+	newApp.Status = "ok"
+	updateCheck := newApp.AddUpdateCheck()
+	updateCheck.Status = "ok"
+	updateCheck.AddUrl("http://10.0.2.2:8080/file?id=")
+	//updateCheck.AddUrl("http://coreos-update.protorz.net:8080/coreos:latest/")
+	manifest := updateCheck.AddManifest("1.0.2")
+	manifest.AddPackage(sha1, id, strconv.FormatInt(size, 10), true)
+	action := manifest.AddAction("postinstall")
+	action.Sha256 = sha256
+	action.DisablePayloadBackoff = true
+	//action.MetadataSignatureRsa = "ixi6Oebo"
+	//action.MetadataSize = "190"
+
+	//data, err := xml.Marshal(resp)
+	data, err := xml.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		log.Error(err.Error())
+		http.Error(w, "An internal error occured while marshalling a response", 500)
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(data)
 }
--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"encoding/xml"
-	"fmt"
 	log "github.com/Sirupsen/logrus"
-	"github.com/coreos/go-omaha/omaha"
 	_ "github.com/mattn/go-sqlite3"
-	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
 	"path"
-	"strconv"
 	"time"
 )
 
@@ -51,79 +46,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	http.HandleFunc("/file", coreosHandler)
-	http.HandleFunc("/update", rootHandler)
+	http.HandleFunc("/file", fileHandler)
+	http.HandleFunc("/update", updateHandler)
 	http.HandleFunc("/shutdown", shutdownHandler)
 	//http.HandleFunc("/admin/add_group", addGroupHandler)
 	http.HandleFunc("/admin/add_payload", addPayloadHandler)
 	//http.HandleFunc("/admin/add_user", addUserHandler)
 	http.HandleFunc("/", homeHandler)
 	http.ListenAndServe(":8080", nil)
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-
-	log.Infof("Handling an update request from %v", r.RemoteAddr)
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		log.Error(err.Error())
-	}
-
-	var reqStructure omaha.Request
-	log.Debugf("%v", string(body[:len(body)]))
-	err = xml.Unmarshal(body, &reqStructure)
-
-	if n := len(reqStructure.Apps); n != 1 {
-		log.Errorf("Client '%v' tried to update %v services", r.RemoteAddr, n)
-		http.Error(w, "I can handle only 1 app update.", 400)
-		return
-	}
-
-	log.Debugf("%#v", reqStructure)
-	applicationUpdate(w, r, reqStructure.Apps[0])
-
-}
-
-func applicationUpdate(w http.ResponseWriter, r *http.Request, app *omaha.App) {
-	if app.Id != coreOSAppID {
-		s := fmt.Sprintf("Unknown app ID '%v'.", app.Id)
-		log.Errorf("Client '%v' tried to update service '%v'.", r.RemoteAddr, app.Id)
-		http.Error(w, s, 400)
-		return
-	}
-
-	payload := backend.GetPayload("user", "channel", "version")
-
-	size := payload.Size
-	sha1 := payload.SHA1
-	sha256 := payload.SHA256
-	id := payload.Url
-
-	resp := omaha.NewResponse("coreos-update.protonet.info")
-	newApp := resp.AddApp(coreOSAppID)
-	newApp.Status = "ok"
-	updateCheck := newApp.AddUpdateCheck()
-	updateCheck.Status = "ok"
-	updateCheck.AddUrl("http://10.0.2.2:8080/file?id=")
-	//updateCheck.AddUrl("http://coreos-update.protorz.net:8080/coreos:latest/")
-	manifest := updateCheck.AddManifest("1.0.2")
-	manifest.AddPackage(sha1, id, strconv.FormatInt(size, 10), true)
-	action := manifest.AddAction("postinstall")
-	action.Sha256 = sha256
-	action.DisablePayloadBackoff = true
-	//action.MetadataSignatureRsa = "ixi6Oebo"
-	//action.MetadataSize = "190"
-
-	//data, err := xml.Marshal(resp)
-	data, err := xml.MarshalIndent(resp, "", "  ")
-	if err != nil {
-		log.Error(err.Error())
-		http.Error(w, "An internal error occured while marshalling a response", 500)
-	}
-
-	w.Header().Set("Content-Type", "application/xml")
-	w.Write(data)
 }
